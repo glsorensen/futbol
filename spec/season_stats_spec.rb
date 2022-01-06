3a4302fbfb4b51e26e307053e432706dba1e8fe6
Conor <>
2022-01-05 18:40:14 -0700

    Test: get_coaches_arr

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -1,9 +1,9 @@
 require './lib/season_stats'
 require 'csv'
 RSpec.describe do
-  let(:game_path) {'./data/games.csv'}
+  let(:game_path) {'./data/games_sample.csv'}
   let(:team_path) {'./data/teams.csv'}
-  let(:game_teams_path) {'./data/game_teams.csv'}
+  let(:game_teams_path) {'./data/game_teams_sample.csv'}
   let(:baby_games) {'./data/baby_games.csv'}
   let(:baby_data) {'./data/baby_data.csv'}
   let(:locations) {{
@@ -34,11 +34,15 @@ RSpec.describe do
 
   it "gets rows based on season" do
     season_stats.games_in_season(baby_games, "20122013")
-    expect(season_stats.get_season_rows(baby_data, :game_id, "20122013").size).to eq(2)
+    expect(season_stats.get_season_rows(baby_data, :game_id).size).to eq(2)
   end
 
-  it "get_coaches_arr" do
+  it "gets coaches from array" do
+    season_stats.games_in_season(game_path, "20122013")
+    season_stats.get_season_rows(game_teams_path, :game_id)
+    expected = ["John Tortorella", "Claude Julien", "Mike Babcock", "Joel Quenneville", "Mike Yeo"]
 
+    expect(season_stats.get_coaches_arr).to eq(expected)
   end
   xit "winningest_coach" do
 
