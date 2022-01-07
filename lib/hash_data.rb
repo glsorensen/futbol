6e787f5bd62b4c4479c6b396e6e3570816256751
Tamara Dowis <tamara.dowis@gmail.com>
2022-01-06 20:51:26 -0600

    Feat: hash_data rspec test

--- lib/hash_data.rb
+++ lib/hash_data.rb
@@ -1,10 +1,14 @@
-require './spec/spec_helper'
+require 'csv'
+require './lib/games.rb'
+require './lib/teams.rb'
+require './lib/game_teams.rb'
 
 class HashData
   attr_reader :games, :teams, :game_teams
+
   def initialize(data)
-    @games = CSV.read(data [:games], headers: true, header_converters: :symbol).map {|row| Games.new(row)}
-    @teams = CSV.read(data [:games], headers: true, header_converters: :symbol).map {|row| Teams.new(row)}
-    @game_teams = CSV.read(data [:games], headers: true, header_converters: :symbol).map {|row| GameTeams.new(row)}
+    @games = CSV.read(data[:games], headers: true, header_converters: :symbol).map {|row| Games.new(row)}
+    @teams = CSV.read(data[:teams], headers: true, header_converters: :symbol).map {|row| Teams.new(row)}
+    @game_teams = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).map {|row| GameTeams.new(row)}
   end
 end
--- spec/hash_data_spec.rb
+++ spec/hash_data_spec.rb
@@ -1,11 +1,21 @@
-require './spec/spec_helper'
+require './lib/hash_data.rb'
+require 'pry'
 
 
 RSpec.describe 'HashData' do
- let(:) {HashData.new}
+  let(:games_data) {'./data/games.csv'}
+  let(:teams_data) {'./data/teams.csv'}
+  let(:game_teams_data) {'./data/game_teams.csv'}
+  let(:data) {{
+        :games => games_data,
+        :teams => teams_data,
+        :game_teams => game_teams_data
+    }}
+
+  let(:hash_data) {HashData.new(data)}
 
   it 'exists' do
-
-    expect().to be_a(HashData)
+# binding.pry
+    expect(hash_data).to be_a(HashData)
   end
 end
