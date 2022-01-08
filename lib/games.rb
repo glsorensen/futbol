bcd69f96ac17a224dadcc2aa68d22bc6731b0f9c
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-08 10:45:22 -0700

    Refactor: Changed instance variables to reflect integers

--- lib/games.rb
+++ lib/games.rb
@@ -1,5 +1,6 @@
-
+require 'csv'
 class Games
+
 attr_reader :game_id,
             :season,
             :type,
@@ -11,6 +12,7 @@ attr_reader :game_id,
             :venue,
             :venue_link
 
+
   def initialize(data)
     @game_id = data[:game_id]
     @season = data[:season]
@@ -18,8 +20,8 @@ attr_reader :game_id,
     @date_time = data[:date_time]
     @away_team_id = data[:away_team_id]
     @home_team_id = data[:home_team_id]
-    @away_goals = data[:away_goals]
-    @home_goals = data[:home_goals]
+    @away_goals = data[:away_goals].to_i
+    @home_goals = data[:home_goals].to_i
     @venue = data[:venue]
     @venue_link = data[:venue_link]
   end
