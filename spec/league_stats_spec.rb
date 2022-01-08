3aa354840b0b0ff61e1254572bf11ff061431da4
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-07 17:38:40 -0700

    Test: test 1 & 2 pass, it exists and can count teams

--- spec/league_stats_spec.rb
+++ spec/league_stats_spec.rb
@@ -1,10 +1,27 @@
-require './spec/spec_helper'
+require 'csv'
+require './lib/games.rb'
+require './lib/teams.rb'
+require './lib/game_teams.rb'
+require './lib/league_stats'
 
-RSpec.describe 'League' do
- let(:) {League.new}
+
+RSpec.describe LeagueStats do
+  let(:games) {'./data/games.csv'}
+  let(:teams) {'./data/teams.csv'}
+  let(:game_teams) {'./data/game_teams.csv'}
+  let(:data) {{
+        :games => games,
+        :teams => teams,
+        :game_teams => game_teams
+    }}
+
+  let(:league) {LeagueStats.new(data)}
 
   it 'exists' do
+  expect(league).to be_a(LeagueStats)
+  end
 
-    expect().to be_a(League)
+  it 'can count teams' do
+  expect(league.league_count).to eq(32)
   end
 end
