2210d79f41881b1531c0b5636399a97f87059143
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-09 17:17:47 -0700

    Refactor: updated hashdata initialize method

--- lib/hash_data.rb
+++ lib/hash_data.rb
@@ -4,14 +4,31 @@ require './lib/teams.rb'
 require './lib/game_teams.rb'
 
 class HashData
-  attr_reader :games, :teams, :game_teams
-
+  attr_reader :games, :teams, :game_teams, :hash, :team_names, :hoa, :team_id
   def initialize(data)
-    @hash = {}
-    @games = CSV.read(data[:games], headers: true, header_converters: :symbol).each { |row| @hash[row[:game_id]] = Games.new(row) }
-    @teams = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:team_id]] = Teams.new(row) }
-    @game_teams = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:game_id]] = GameTeams.new(row) }
-  end
+  @hash = {}
+  @team_names = {}
+  @hoa = {}
+  @team_id = {}
+  @games = CSV.read(data[:games], headers: true, header_converters: :symbol).each { |row| @hash[row[:date_time]] = Games.new(row) }
+  @teams = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:team_id]] = Teams.new(row) }
+  @game_teams = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:game_id]] = GameTeams.new(row) }
+  @names = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @team_names[row[:teamname]] = {} }
+  @ho_aw = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).each { |row| @hoa[row[:hoa]] = [] }
+  @id = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @team_id[row[:team_id]] = [row[:teamname]] }
+end
 
+def merge_hash
+  @team_names.transform_values { |value| value = @hoa }
+end
 
 end
+
+
+
+
+
+# @hash = {}
+# @games = CSV.read(data[:games], headers: true, header_converters: :symbol).each { |row| @hash[row[:game_id]] = Games.new(row) }
+# @teams = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:team_id]] = Teams.new(row) }
+# @game_teams = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:game_id]] = GameTeams.new(row) }
