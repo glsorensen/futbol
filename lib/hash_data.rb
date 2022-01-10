42393fafff27ed9031e86a00dc40a975a3ee9d00
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-09 18:44:42 -0700

    Refactor: updated hashdata instance variable names to reflect their job

--- lib/hash_data.rb
+++ lib/hash_data.rb
@@ -4,7 +4,7 @@ require './lib/teams.rb'
 require './lib/game_teams.rb'
 
 class HashData
-  attr_reader :games, :teams, :game_teams, :hash, :team_names, :hoa, :team_id
+  attr_reader :games, :teams, :game_teams, :hash, :team_names, :team_id
   def initialize(data)
   @hash = {}
   @team_names = {}
@@ -13,14 +13,20 @@ class HashData
   @games = CSV.read(data[:games], headers: true, header_converters: :symbol).each { |row| @hash[row[:date_time]] = Games.new(row) }
   @teams = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:team_id]] = Teams.new(row) }
   @game_teams = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).each { |row| @hash[row[:game_id]] = GameTeams.new(row) }
-  @names = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @team_names[row[:teamname]] = {} }
-  @ho_aw = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).each { |row| @hoa[row[:hoa]] = [] }
-  @id = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @team_id[row[:team_id]] = [row[:teamname]] }
+  @get_team_names = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @team_names[row[:teamname]] = {} }
+  @get_home_away = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).each { |row| @hoa[row[:hoa]] = [] }
+  @link_team_id_to_team_name = CSV.read(data[:teams], headers: true, header_converters: :symbol).each { |row| @team_id[row[:team_id]] = row[:teamname] }
 end
 
 def merge_hash
   @team_names.transform_values { |value| value = @hoa }
 end
+
+def populate_goals
+  merge_hash
+
+end
+
 
 end
 
