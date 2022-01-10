597684d2aec977098f71ea186dd9e99d6684f674
Tamara Dowis <tamara.dowis@gmail.com>
2022-01-10 17:29:32 -0600

    Test: can create a hash of games for a slected team

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -25,11 +25,11 @@ class TeamStats < HashData
   def opponent_array(team_id)
     ab = games_filtered(team_id)
     opponent_array = []
-    ab.filter.map do |game|
-      opponent_array << game.home_team_id if game.home_team_id != team_id
-      opponent_array << game.away_team_id if game.away_team_id != team_id
-    end
-    opponent_array.uniq
+    ab.select {|game| opponent_array << game.away_team_id}
+    ab.select {|game| opponent_array << game.home_team_id}
+    c = opponent_array.uniq.sort
+    c.delete(team_id)
+    c
   end
 
   def average_win_percentage(team_id)
@@ -67,14 +67,11 @@ class TeamStats < HashData
     merged_array = d.zip(e)
   end
 
-
   def hash_team_games_by_season(team_id)
     g = sorted_games_array(team_id)
     flattened_game_array = g.map {|info| info.flatten}
     hashed_by_season = flattened_game_array.group_by {|game| game[1].season}
   end
-
-
 
   def best_season(team_id)
     h = hash_team_games_by_season(team_id)
@@ -102,124 +99,39 @@ class TeamStats < HashData
     max_game.goals.to_i
   end
 
-  def  fewest_goals_scored(team_id)
+  def fewest_goals_scored(team_id)
     a = game_teams_filtered(team_id)
     min_game = a.min_by {|game| game.goals}
     min_game.goals.to_i
   end
 
-  def favorite_opponent(team_id)
-  #   ab = opponent_array(team_id)
-  #   opponent_awp_hash = {}
-  #   calculate_opponent_average_win_percentage = ab.map do |o_team|
-  #     opponent_awp_hash[o_team] = o_team.average_win_percentage
-  #   end
-  # #
-  # binding.pry
-  # end
-    # opponent_array = []
-    # @games.map do |game|
-    #   opponent_array << unless game.away_team_id || game.home_team_id
-
+  def select_team_games_hash(team_id)
+    ab = games_filtered(team_id)
+    clean_array = opponent_array(team_id)
+    games_hash = Hash[clean_array.map {|oti| [oti, Array.new]}]
+    add_away_games = ab.each do |game|
+      if games_hash.has_key?(game.away_team_id) ; games_hash[game.away_team_id] << game
+    end
+  end
+    add_home_games = ab.each do |game|
+      if games_hash.has_key?(game.home_team_id) ; games_hash[game.home_team_id] << game
+    end
+  games_hash
   end
 end
 
-  # return unless ship_fits_within?(ship, selected_coordinates)
-  #   # TODO: rename valid_placement_overlapping
-  #   return unless valid_placement_overlapping?(ship, selected_coordinates))
-  #   return true if abc_placement?(ship, selected_coordinates)
-  #   return true if numerical_placement?(ship, selected_coordinates)
-
-# games_by_season = b.filter do |season, games|
-#   games.filter {|game| game_id.include?(game.game_id)}
-# game_teams_filtered = @game_teams.find_all {|team| team.team_id == team_id}
-# game_ids = game_teams_filtered.map {|game| game.game_id1}
-# go = TeamStatistics.new
-#
-# binding.pry
-#
-# p
-
-# def average_win_hash_percentage(games)
-#   total_games = games.count
-#   wins = games.count { |game| game.result == "WIN"}
-#   awhp = (wins.to_f / total_games).to_f.round(2)
-# end
-# team_games = @game_teams.find_all { |game| game.team_id == team_id}
-# team_games_by_season = team_games.group_by { |game| game.season}
-# losses = team_games.count { |game| game.result == "LOSS"}
-# ties = team_games.count { |game| game.result == "TIES"}
-# print " W = #{wins} L = #{losses}, T = #{ties}"
-# def hash_by(csv, header_to_key)
-#     parse(csv).group_by do |row|
-#     row[header_to_key]
-#   end
-# end
-#
-# def count_hashed_by(csv, header_to_key, specific_key, counted_value)
-# hashed = hash_by(csv, header_to_key)
-# select_team = hashed.select {|k,v| k == specific_key }
-# count = 0
-# select_team.each do |key, value|
-#   value.map do |row|
-#     count += 1 if row[3] == counted_value
-#   end
-#   return count
-# end
-# end
-#
-# def team_info
-#   teams = CSV.read './data/teams.csv', headers: true, header_converters: :symbol
-#   team_hash = teams.map do |row|
-#     row.to_h
-#   end
-#   team_hash_reduced = team_hash.each do |team|
-#     team.delete_if {|key, value| key == :stadium}
-#   end
-# end
-# @seasons_hashed = @games.group_by { |game| game.game_id }
-# (people1 + people2)
-#   .group_by { |person| person[:name] }
-#   .map(&:last)
-#   .select { |people| people.count > 1 }
-
-#   team_hash = teams.map do |row|
-#     row.to_h
-#   end
-#   team_hash_reduced = team_hash.each do |team|
-#     team.delete_if {|key, value| key == :stadium}
-#   teams = CSV.read './data/teams.csv', headers: true, header_converters: :symbol
-#   team_hash = teams.map do |row|
-#     row.to_h
-#   end
-#   team_hash_reduced = team_hash.each do |team|
-#     team.delete_if {|key, value| key == :stadium}
-    # teams_grouped = @teams.group_by {|team| team.team_id }
-    # choosen = teams_grouped.select {|team, info| team == team_id}
-    # team_hash = @teams.map do |row|
-    #     row.to_h
-    #   end
-    # all_team_info = @teams
-    # select_team = all_team_info.map do {|team| team.team_id == team_id}
-    # required_team_info = select_team.each do |team|
-    #   team.delete_if {|key, value| key == :stadium}
-
-        # game_teams_filtered_season = game_teams_filtered.group_by { |game| game.game_id[0..3]}
-        # calculate_each_season = game_teams_filtered_season.each do |season, games|
-        #   season.count {|game| game.result == "WIN"}
-        #   end.count
-
-
-        # what_seasons = @games.map do { |game| game.game_id == game_teams_filtered.each do |game| game.game_id1}
-
-
-
-          # => game_teams_filtered.map do {|game| game.game_id == what_seasons.key}
-
-        # g
-
-        # game_ids = game_teams_filtered.map {|game| game.game_id}.sort
-        # what_seasons = @games.find_all { |game| game_teams_filtered.game_id == game.game_id}
-        # seasons = @games.group_by { |game| game.season}
-        # teams = @game_teams.group_by { |team| team.team_id}
-        # team_game_by_season = group_by do {| season, games| games.home_team_id || games.away_team_id == team_id}
+  # def favorite_opponent(team_id)
+  #   ab = games_filtered(team_id)
+  #   clean_array = opponent_array(team_id)
+  #   games_hash = Hash[clean_array.map {|oti| [oti, Array.new]}]
+  #   add_away_games = ab.each do |game|
+  #     if games_hash.has_key?(game.away_team_id) ; games_hash[game.away_team_id] << game
+  #   end
+  # end
+  #   add_home_games = ab.each do |game|
+  #     if games_hash.has_key?(game.home_team_id) ; games_hash[game.home_team_id] << game
+  #   end
+  # games_hash
+  #   # binding.pry
+  # end
+end
--- spec/team_stats_spec.rb
+++ spec/team_stats_spec.rb
@@ -13,12 +13,12 @@ RSpec.describe TeamStats do
     }}
     let(:team) {TeamStats.new(data)}
 
-  it 'exists' do
+  xit 'exists' do
 
     expect(team).to be_a(TeamStats)
   end
 
-  it 'test 1 * team will hash requested team info' do
+  xit 'test 1 * team will hash requested team info' do
     expected = {
       "team_id" => "18",
       "franchise_id" => "34",
@@ -30,21 +30,21 @@ RSpec.describe TeamStats do
     expect(team.team_info('18')).to eq(expected)
   end
 
-  it 'filter games_teams by team_id' do
+  xit 'filter games_teams by team_id' do
 
     expect(team.game_teams_filtered('6').size).to eq 510
   end
 
-  it 'filter games by team id' do
+  xit 'filter games by team id' do
     expect(team.games_filtered('18').size).to eq 513
   end
 
-  it 'array of games ids played' do
+  xit 'array of games ids played' do
 
     expect(team.game_ids('6').size).to eq 510
   end
 
-  it 'can filter game_teams using game ids array' do
+  xit 'can filter game_teams using game ids array' do
 
     expect(team.games_filter_game_id('6').size).to eq 510
   end
@@ -53,78 +53,83 @@ RSpec.describe TeamStats do
     expect(team.opponent_array('18').size).to eq 31
   end
 
-  it 'can caculate hash of opponent average win percentage' do
+  xit 'can caculate hash of opponent average win percentage' do
     expected = {"19"=>0.45,
- "52"=>0.4,
- "21"=>0.37,
- "20"=>0.37,
- "17"=>0.38,
- "29"=>0.4,
- "25"=>0.39,
- "16"=>0.44,
- "30"=>0.42,
- "1"=>0.36,
- "8"=>0.42,
- "23"=>0.37,
- "3"=>0.43,
- "14"=>0.42,
- "15"=>0.46,
- "28"=>0.45,
- "22"=>0.33,
- "24"=>0.45,
- "5"=>0.48,
- "2"=>0.36,
- "26"=>0.45,
- "7"=>0.28,
- "27"=>0.34,
- "6"=>0.49,
- "13"=>0.35,
- "10"=>0.34,
- "9"=>0.35,
- "12"=>0.38,
- "54"=>0.5,
- "4"=>0.34,
- "53"=>0.29}
+       "52"=>0.4,
+       "21"=>0.37,
+       "20"=>0.37,
+       "17"=>0.38,
+       "29"=>0.4,
+       "25"=>0.39,
+       "16"=>0.44,
+       "30"=>0.42,
+       "1"=>0.36,
+       "8"=>0.42,
+       "23"=>0.37,
+       "3"=>0.43,
+       "14"=>0.42,
+       "15"=>0.46,
+       "28"=>0.45,
+       "22"=>0.33,
+       "24"=>0.45,
+       "5"=>0.48,
+       "2"=>0.36,
+       "26"=>0.45,
+       "7"=>0.28,
+       "27"=>0.34,
+       "6"=>0.49,
+       "13"=>0.35,
+       "10"=>0.34,
+       "9"=>0.35,
+       "12"=>0.38,
+       "54"=>0.5,
+       "4"=>0.34,
+       "53"=>0.29}
     expect(team.opponent_awp_hash('18')).to eq expected
   end
 
-  it 'can create a merged array of game_teams and games ' do
+  xit 'can create a merged array of game_teams and games ' do
 
     expect(team.sorted_games_array('6').size).to eq 510
   end
 
-  it 'test 4* can calculate average_win_percentage' do
+  xit 'test 4* can calculate average_win_percentage' do
 
      expect(team.average_win_percentage('6')).to eq(0.49)
    end
 
-   it 'can hash a team games by season' do
+   xit 'can hash a team games by season' do
 
      expect(team.hash_team_games_by_season('18').size).to eq(6)
    end
 
-   it 'test 2* can calculate a teams best_season' do
+   xit 'test 2* can calculate a teams best_season' do
 
      expect(team.best_season("6")).to eq "20132014"
    end
 
-   it 'test 3* can calculate a teams worst_season' do
+   xit 'test 3* can calculate a teams worst_season' do
 
      expect(team.worst_season("6")).to eq "20142015"
    end
 
 
-   it 'test 5* can calculate the most goals scored for a team' do
+   xit 'test 5* can calculate the most goals scored for a team' do
 
      expect(team.most_goals_scored("18")).to eq 7
    end
 
-   it 'test 6* can calculate the least goals scored for a team' do
+   xit 'test 6* can calculate the least goals scored for a team' do
 
      expect(team.fewest_goals_scored("18")).to eq 0
    end
 
+   it 'can create a hash of a given teams games by team' do
+     expect(team.select_team_games_hash("18").size).to eq 31
+   end
+
    xit 'can calculate a teams favorite_opponent' do
+
      expect(team.favorite_opponent("18")).to eq "DC United"
    end
 end
