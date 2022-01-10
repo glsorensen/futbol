b4be26d99b93fc204a2b3c85aa49c292c98fd966
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-01-09 18:13:48 -0700

    Refactor: game_stats working on stat tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,49 +1,50 @@
-require './spec/spec_helper'
+# require './spec/spec_helper'
+require './lib/game_stats.rb'
+require './lib/hash_data.rb'
 
 class StatTracker
 
   def initialize(data_location)
     @hash_data = HashData.new(data_location)
-    # @game_stats = GameStats.new
+    @game_stats = GameStats.new(data_location)
     @league_stats = LeagueStats.new(data_location)
     # @team_stats = TeamStats.new
     # @season_stats = SeasonStats.new
-
   end
 
-    def highest_total_score
-      #game_statistic
-    end
+  def highest_total_score
+   @game_stats.highest_total_score
+ end
 
-    def lowest_total_score
-      #game_statistic
-    end
+ def lowest_total_score
+   @game_stats.lowest_total_score
+ end
 
-    def percentage_home_wins
-      #game_statistic
-    end
+ def percentage_home_wins
+   @game_stats.percentage_home_wins
+ end
 
-    def percentage_visitor_wins
-      #game_statistic
-    end
+ def percentage_visitor_wins
+   @game_stats.percentage_visitor_wins
+ end
 
-    def percentage_ties
-      #game_statistic
-    end
+ def percentage_ties
+   @game_stats.percentage_ties
+ end
 
-    def count_of_games_by_season
-      #game_statistic
-    end
+ def count_of_games_by_season
+   @game_stats.count_of_games_by_season
+ end
 
-    def average_goals_per_game
-      #game_statistic
-    end
+ def average_goals_per_game
+   @game_stats.average_goals_per_game
+ end
 
-    def average_goals_by_season
-      #game_statistic
-    end
+ def average_goals_by_season
+   #game_statistic
+ end
 
-    def count_of_teams
+   def count_of_teams
       @league_stats.league_count
     end
 
