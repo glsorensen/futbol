ce2a24c8a1f5d12a60d0cc4fdb03bb3e840084d6
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-10 09:52:40 -0700

    Fix: Clean up sta tracker methods, indents are correct

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -44,88 +44,88 @@ class StatTracker
    #game_statistic
  end
 
-   def count_of_teams
-      @league_stats.league_count
-    end
+  def count_of_teams
+    @league_stats.league_count
+  end
 
-    def best_offense
-      #league_statistics
-    end
+  def best_offense
+    #league_statistics
+  end
 
-    def worst_offense
-      #league_statistics
-    end
+  def worst_offense
+    #league_statistics
+  end
 
-    def highest_scoring_visitor
-      #league_statistics
-    end
+  def highest_scoring_visitor
+    #league_statistics
+  end
 
-    def highest_scoring_home_team
-      #league_statistics
-    end
+  def highest_scoring_home_team
+    #league_statistics
+  end
 
-    def lowest_scoring_visitor
-      #league_statistics
-    end
+  def lowest_scoring_visitor
+    #league_statistics
+  end
 
-    def lowest_scoring_home_team
-      #league_statistics
-    end
+  def lowest_scoring_home_team
+    #league_statistics
+  end
 
-    def winningest_coach(season_id)
-      @season_stats.winningest_coach(season_id)
-    end
+  def winningest_coach(season_id)
+    @season_stats.winningest_coach(season_id)
+  end
 
-    def worst_coach
-      @season_stats.losingest_coach(season_id)
-    end
+  def worst_coach
+    @season_stats.losingest_coach(season_id)
+  end
 
-    def most_accurrate_team
-      @season_stats.scoringest_team(season_id)
-    end
+  def most_accurrate_team
+    @season_stats.scoringest_team(season_id)
+  end
 
-    def least_accurate_team
-      @season_stats.missingest_team(season_id)
-    end
+  def least_accurate_team
+    @season_stats.missingest_team(season_id)
+  end
 
-    def most_tackles
-      @season_stats.tackliest_team(season_id)
-    end
+  def most_tackles
+    @season_stats.tackliest_team(season_id)
+  end
 
-    def fewest_tackles
-      @season_stats.untackliest_team(season_id)
-    end
+  def fewest_tackles
+    @season_stats.untackliest_team(season_id)
+  end
 
-    def team_info
-      #team_statistics
-    end
+  def team_info
+    #team_statistics
+  end
 
-    def best_season
-      #team_statistics
-    end
+  def best_season
+    #team_statistics
+  end
 
-    def worst_season
-      #team_statistics
-    end
+  def worst_season
+    #team_statistics
+  end
 
-    def average_win_percentage
-      #team_statistics
-    end
+  def average_win_percentage
+    #team_statistics
+  end
 
-    def most_goals_scored
-      #team_statistics
-    end
+  def most_goals_scored
+    #team_statistics
+  end
 
-    def fewest_goals_scored
-      #team_statistics
-    end
+  def fewest_goals_scored
+    #team_statistics
+  end
 
-    def favorite_opponent
-      #team_statistics
-    end
+  def favorite_opponent
+    #team_statistics
+  end
 
-    def rival
-      #team_statistics
-    end
+  def rival
+    #team_statistics
+  end
 
 end
